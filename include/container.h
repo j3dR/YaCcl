c5b7259222639167f284d3ac5a57cd81ea117e47
j3dR <j3dr07@gmail.com>
2023-09-10 14:19:15 +0200

    Add char, short, long and double data types

--- include/container.h
+++ include/container.h
@@ -7,8 +7,12 @@
 const char* const LIB_NAME = "container.h";  // library name
 
 enum ContainerDataType {
+	CHAR,
+	SHORT,
 	INT,
-	FLOAT
+	LONG,
+	FLOAT,
+	DOUBLE
 };
 
 // Template
@@ -31,16 +35,28 @@ Container* container_new(size_t size, enum ContainerDataType data_type) {
 	container->data_type = data_type;
 
 	switch (data_type) {
+		case CHAR:
+			container->data = calloc(size, sizeof(char));
+			break;
+		case SHORT:
+			container->data = calloc(size, sizeof(short));
+			break;
 		case INT:
 			container->data = calloc(size, sizeof(int));
+			break;
+		case LONG:
+			container->data = calloc(size, sizeof(long));
 			break;
 		case FLOAT:
 			container->data = calloc(size, sizeof(float));
 			break;
+		case DOUBLE:
+			container->data = calloc(size, sizeof(double));
+			break;
 		default:
 			free(container);
 			fprintf(stderr, "%s error: container_new: Data type not supported\n", LIB_NAME);
-			puts("                                  Supported data types: int, float");
+			puts("                                  Supported data types: char, short, int, long, float, double");
 			return NULL;
 			break;
 	}
@@ -107,6 +123,34 @@ void container_remove(Container** container, size_t index) {
 
 	size_t data_size;
 	switch ((*container)->data_type) {
+		case CHAR:
+			data_size = sizeof(char);
+
+			if (index == (size_t)0) {
+				for (size_t i = 0; i < (*container)->size - 1; ++i) {
+					((char*)(*container)->data)[i] = ((char*)(*container)->data)[i + 1];
+				}
+			}
+			else if (index < (*container)->size - 1) {
+				for (size_t i = index; i < (*container)->size - 1; ++i) {
+					((char*)(*container)->data)[i] = ((char*)(*container)->data)[i + 1];
+				}
+			}
+			break;
+		case SHORT:
+			data_size = sizeof(short);
+
+			if (index == (size_t)0) {
+				for (size_t i = 0; i < (*container)->size - 1; ++i) {
+					((short*)(*container)->data)[i] = ((short*)(*container)->data)[i + 1];
+				}
+			}
+			else if (index < (*container)->size - 1) {
+				for (size_t i = index; i < (*container)->size - 1; ++i) {
+					((short*)(*container)->data)[i] = ((short*)(*container)->data)[i + 1];
+				}
+			}
+			break;
 		case INT:
 			data_size = sizeof(int);
 
@@ -118,6 +162,20 @@ void container_remove(Container** container, size_t index) {
 			else if (index < (*container)->size - 1) {
 				for (size_t i = index; i < (*container)->size - 1; ++i) {
 					((int*)(*container)->data)[i] = ((int*)(*container)->data)[i + 1];
+				}
+			}
+			break;
+		case LONG:
+			data_size = sizeof(long);
+
+			if (index == (size_t)0) {
+				for (size_t i = 0; i < (*container)->size - 1; ++i) {
+					((long*)(*container)->data)[i] = ((long*)(*container)->data)[i + 1];
+				}
+			}
+			else if (index < (*container)->size - 1) {
+				for (size_t i = index; i < (*container)->size - 1; ++i) {
+					((long*)(*container)->data)[i] = ((long*)(*container)->data)[i + 1];
 				}
 			}
 			break;
@@ -135,6 +193,20 @@ void container_remove(Container** container, size_t index) {
 				}
 			}
 			break;
+		case DOUBLE:
+			data_size = sizeof(double);
+
+			if (index == (size_t)0) {
+				for (size_t i = 0; i < (*container)->size - 1; ++i) {
+					((double*)(*container)->data)[i] = ((double*)(*container)->data)[i + 1];
+				}
+			}
+			else if (index < (*container)->size - 1) {
+				for (size_t i = index; i < (*container)->size - 1; ++i) {
+					((double*)(*container)->data)[i] = ((double*)(*container)->data)[i + 1];
+				}
+			}
+			break;
 	}
 
 	(*container)->data = realloc((*container)->data, --(*container)->size * data_size);
@@ -147,8 +219,62 @@ void container_remove(Container** container, size_t index) {
 // Assign functions --------------------------------------------------------------------------------------------------------------
 
 #define container_assign(container_reference, value, size_t_index) _Generic(value,                                              \
+														   char: container_assignc(container_reference, value, size_t_index),   \
+														   short: container_assigns(container_reference, value, size_t_index),  \
 														   int: container_assigni(container_reference, value, size_t_index),    \
-														   float: container_assignf(container_reference, value, size_t_index))  \
+														   long: container_assignl(container_reference, value, size_t_index),   \
+														   float: container_assignf(container_reference, value, size_t_index),  \
+														   double: container_assignd(container_reference, value, size_t_index)) \
+
+// Assign char
+void container_assignc(Container** container, char value, size_t index) {
+	if (*container == NULL) {
+		fprintf(stderr, "%s error: container_assign: Container is non-existent\n", LIB_NAME);
+		return;
+	}
+
+	if ((*container)->data == NULL) {
+		fprintf(stderr, "%s error: container_assign: Data is empty\n", LIB_NAME);
+		return;
+	}
+
+	if ((*container)->data_type != CHAR) {
+		fprintf(stderr, "%s error: container_assign: Incorrect value data type\n", LIB_NAME);
+		return;
+	}
+
+	if (index > (*container)->size - 1) {
+		fprintf(stderr, "%s error: container_assign: Incorrect index\n", LIB_NAME);
+		return;
+	}
+
+	((char*)(*container)->data)[index] = value;
+}
+
+// Assign short
+void container_assigns(Container** container, short value, size_t index) {
+	if (*container == NULL) {
+		fprintf(stderr, "%s error: container_assign: Container is non-existent\n", LIB_NAME);
+		return;
+	}
+
+	if ((*container)->data == NULL) {
+		fprintf(stderr, "%s error: container_assign: Data is empty\n", LIB_NAME);
+		return;
+	}
+
+	if ((*container)->data_type != SHORT) {
+		fprintf(stderr, "%s error: container_assign: Incorrect value data type\n", LIB_NAME);
+		return;
+	}
+
+	if (index > (*container)->size - 1) {
+		fprintf(stderr, "%s error: container_assign: Incorrect index\n", LIB_NAME);
+		return;
+	}
+
+	((short*)(*container)->data)[index] = value;
+}
 
 // Assign int
 void container_assigni(Container** container, int value, size_t index) {
@@ -173,6 +299,31 @@ void container_assigni(Container** container, int value, size_t index) {
 	}
 
 	((int*)(*container)->data)[index] = value;
+}
+
+// Assign long
+void container_assignl(Container** container, long value, size_t index) {
+	if (*container == NULL) {
+		fprintf(stderr, "%s error: container_assign: Container is non-existent\n", LIB_NAME);
+		return;
+	}
+
+	if ((*container)->data == NULL) {
+		fprintf(stderr, "%s error: container_assign: Data is empty\n", LIB_NAME);
+		return;
+	}
+
+	if ((*container)->data_type != LONG) {
+		fprintf(stderr, "%s error: container_assign: Incorrect value data type\n", LIB_NAME);
+		return;
+	}
+
+	if (index > (*container)->size - 1) {
+		fprintf(stderr, "%s error: container_assign: Incorrect index\n", LIB_NAME);
+		return;
+	}
+
+	((long*)(*container)->data)[index] = value;
 }
 
 // Assign float
@@ -200,7 +351,70 @@ void container_assignf(Container** container, float value, size_t index) {
 	((float*)(*container)->data)[index] = value;
 }
 
+// Assign double
+void container_assignd(Container** container, double value, size_t index) {
+	if (*container == NULL) {
+		fprintf(stderr, "%s error: container_assign: Container is non-existent\n", LIB_NAME);
+		return;
+	}
+
+	if ((*container)->data == NULL) {
+		fprintf(stderr, "%s error: container_assign: Data is empty\n", LIB_NAME);
+		return;
+	}
+
+	if ((*container)->data_type != DOUBLE) {
+		fprintf(stderr, "%s error: container_assign: Incorrect value data type\n", LIB_NAME);
+		return;
+	}
+
+	if (index > (*container)->size - 1) {
+		fprintf(stderr, "%s error: container_assign: Incorrect index\n", LIB_NAME);
+		return;
+	}
+
+	((double*)(*container)->data)[index] = value;
+}
+
 // Getters -----------------------------------------------------------------------------------------------------------------------
+
+char container_getc(Container* container, size_t index) {
+	if (container == NULL) {
+		fprintf(stderr, "%s error: container_geti: Container is non-existent\n", LIB_NAME);
+		return 0;
+	}
+
+	if (container->data == NULL) {
+		fprintf(stderr, "%s error: container_geti: Data is empty\n", LIB_NAME);
+		return 0;
+	}
+
+	if (index > container->size - 1) {
+		fprintf(stderr, "%s error: container_geti: Incorrect index\n", LIB_NAME);
+		return 0;
+	}
+
+	return ((char*)container->data)[index];
+}
+
+short container_gets(Container* container, size_t index) {
+	if (container == NULL) {
+		fprintf(stderr, "%s error: container_geti: Container is non-existent\n", LIB_NAME);
+		return 0;
+	}
+
+	if (container->data == NULL) {
+		fprintf(stderr, "%s error: container_geti: Data is empty\n", LIB_NAME);
+		return 0;
+	}
+
+	if (index > container->size - 1) {
+		fprintf(stderr, "%s error: container_geti: Incorrect index\n", LIB_NAME);
+		return 0;
+	}
+
+	return ((short*)container->data)[index];
+}
 
 int container_geti(Container* container, size_t index) {
 	if (container == NULL) {
@@ -219,6 +433,25 @@ int container_geti(Container* container, size_t index) {
 	}
 
 	return ((int*)container->data)[index];
+}
+
+long container_getl(Container* container, size_t index) {
+	if (container == NULL) {
+		fprintf(stderr, "%s error: container_geti: Container is non-existent\n", LIB_NAME);
+		return 0;
+	}
+
+	if (container->data == NULL) {
+		fprintf(stderr, "%s error: container_geti: Data is empty\n", LIB_NAME);
+		return 0;
+	}
+
+	if (index > container->size - 1) {
+		fprintf(stderr, "%s error: container_geti: Incorrect index\n", LIB_NAME);
+		return 0;
+	}
+
+	return ((long*)container->data)[index];
 }
 
 float container_getf(Container* container, size_t index) {
@@ -240,12 +473,102 @@ float container_getf(Container* container, size_t index) {
 	return ((float*)container->data)[index];
 }
 
+double container_getd(Container* container, size_t index) {
+	if (container == NULL) {
+		fprintf(stderr, "%s error: container_geti: Container is non-existent\n", LIB_NAME);
+		return 0;
+	}
+
+	if (container->data == NULL) {
+		fprintf(stderr, "%s error: container_geti: Data is empty\n", LIB_NAME);
+		return 0;
+	}
+
+	if (index > container->size - 1) {
+		fprintf(stderr, "%s error: container_geti: Incorrect index\n", LIB_NAME);
+		return 0;
+	}
+
+	return ((double*)container->data)[index];
+}
+
 // Append functions --------------------------------------------------------------------------------------------------------------
 
 #define container_append(container_reference, value) _Generic(value,                                       \
+												    char: container_appendc(container_reference, value),   \
+													short: container_appends(container_reference, value),  \
 													int: container_appendi(container_reference, value),    \
-													float: container_appendf(container_reference, value))  \
+													long: container_appendl(container_reference, value),   \
+													float: container_appendf(container_reference, value),  \
+													double: container_appendd(container_reference, value)) \
 
+// Append char
+void container_appendc(Container** container, char value) {
+	if (*container == NULL) {
+		*container = container_new(1, CHAR);
+		if (*container == NULL) {
+			fprintf(stderr, "%s error: container_append: Failed to initialize empty container\n", LIB_NAME);
+			return;
+		}
+
+		((char*)(*container)->data)[0] = value;
+	}
+	else if ((*container)->data == NULL) {
+		(*container)->size = 1;
+
+		(*container)->data = malloc(sizeof(char));
+		if ((*container)->data == NULL) {
+			fprintf(stderr, "%s error: container_append: Failed to allocate data\n", LIB_NAME);
+			return;
+		}
+
+		((char*)(*container)->data)[0] = value;
+	}
+	else {
+		(*container)->data = realloc((*container)->data, ++(*container)->size * sizeof(char));
+		if ((*container)->data == NULL) {
+			fprintf(stderr, "%s error: container_append: Failed to reallocate memory\n", LIB_NAME);
+			return;
+		}
+
+		((char*)(*container)->data)[(*container)->size - 1] = value;
+	}
+}
+
+// Append short
+void container_appends(Container** container, short value) {
+	if (*container == NULL) {
+		*container = container_new(1, SHORT);
+		if (*container == NULL) {
+			fprintf(stderr, "%s error: container_append: Failed to initialize empty container\n", LIB_NAME);
+			return;
+		}
+
+		((short*)(*container)->data)[0] = value;
+	}
+	else if ((*container)->data == NULL) {
+		(*container)->size = 1;
+
+		(*container)->data = malloc(sizeof(short));
+		if ((*container)->data == NULL) {
+			fprintf(stderr, "%s error: container_append: Failed to allocate data\n", LIB_NAME);
+			return;
+		}
+
+		((short*)(*container)->data)[0] = value;
+	}
+	else {
+		(*container)->data = realloc((*container)->data, ++(*container)->size * sizeof(short));
+		if ((*container)->data == NULL) {
+			fprintf(stderr, "%s error: container_append: Failed to reallocate memory\n", LIB_NAME);
+			return;
+		}
+
+		((short*)(*container)->data)[(*container)->size - 1] = value;
+	}
+}
+
+// Append int
 void container_appendi(Container** container, int value) {
 	if (*container == NULL) {
 		*container = container_new(1, INT);
@@ -278,6 +601,40 @@ void container_appendi(Container** container, int value) {
 	}
 }
 
+// Append long
+void container_appendl(Container** container, long value) {
+	if (*container == NULL) {
+		*container = container_new(1, LONG);
+		if (*container == NULL) {
+			fprintf(stderr, "%s error: container_append: Failed to initialize empty container\n", LIB_NAME);
+			return;
+		}
+
+		((long*)(*container)->data)[0] = value;
+	}
+	else if ((*container)->data == NULL) {
+		(*container)->size = 1;
+
+		(*container)->data = malloc(sizeof(long));
+		if ((*container)->data == NULL) {
+			fprintf(stderr, "%s error: container_append: Failed to allocate data\n", LIB_NAME);
+			return;
+		}
+
+		((long*)(*container)->data)[0] = value;
+	}
+	else {
+		(*container)->data = realloc((*container)->data, ++(*container)->size * sizeof(long));
+		if ((*container)->data == NULL) {
+			fprintf(stderr, "%s error: container_append: Failed to reallocate memory\n", LIB_NAME);
+			return;
+		}
+
+		((long*)(*container)->data)[(*container)->size - 1] = value;
+	}
+}
+
+// Append float
 void container_appendf(Container** container, float value) {
 	if (*container == NULL) {
 		*container = container_new(1, FLOAT);
@@ -307,5 +664,38 @@ void container_appendf(Container** container, float value) {
 		}
 
 		((float*)(*container)->data)[(*container)->size - 1] = value;
+	}
+}
+
+// Append double
+void container_appendd(Container** container, double value) {
+	if (*container == NULL) {
+		*container = container_new(1, DOUBLE);
+		if (*container == NULL) {
+			fprintf(stderr, "%s error: container_append: Failed to initialize empty container\n", LIB_NAME);
+			return;
+		}
+
+		((double*)(*container)->data)[0] = value;
+	}
+	else if ((*container)->data == NULL) {
+		(*container)->size = 1;
+
+		(*container)->data = malloc(sizeof(double));
+		if ((*container)->data == NULL) {
+			fprintf(stderr, "%s error: container_append: Failed to allocate data\n", LIB_NAME);
+			return;
+		}
+
+		((double*)(*container)->data)[0] = value;
+	}
+	else {
+		(*container)->data = realloc((*container)->data, ++(*container)->size * sizeof(double));
+		if ((*container)->data == NULL) {
+			fprintf(stderr, "%s error: container_append: Failed to reallocate memory\n", LIB_NAME);
+			return;
+		}
+
+		((double*)(*container)->data)[(*container)->size - 1] = value;
 	}
 }
--- src/main.c
+++ src/main.c
@@ -1,29 +1,55 @@
 #include "../include/container.h"
 
 int main() {
-	Container* c = NULL;
-	container_append(&c, 54);
-	printf("Type: %u\n", c->data_type);
-	printf("Size: %lu\n", c->size);
-	printf("c[0] = %d\n\n", container_geti(c, 0));
+	Container* container = NULL;
+	container_append(&container, 4.567);
+	switch (container->data_type) {
+		case CHAR:
+			puts("Type: char");
+			break;
+		case SHORT:
+			puts("Type: short");
+			break;
+		case INT:
+			puts("Type: int");
+			break;
+		case LONG:
+			puts("Type: long");
+			break;
+		case FLOAT:
+			puts("Type: float");
+			break;
+		case DOUBLE:
+			puts("Type: double");
+			break;
+	}
+	printf("Size: %lu\n", container->size);
+	printf("c[0] = %f\n\n", container_getd(container, 0));
 
 	for (int i = 0; i < 10; ++i) {
-		container_append(&c, i);
+		container_append(&container, 2.345);
 	}
 
-	for (size_t i = 0; i < c->size; ++i) {
-		printf("%d\n", container_geti(c, i));
+	for (size_t i = 0; i < container->size; ++i) {
+		printf("%f\n", container_getd(container, i));
 	}
-	printf("Size: %lu\n\n", c->size);
+	printf("Size: %lu\n\n", container->size);
 
-	container_remove(&c, 9);
+	container_remove(&container, 9);
 
-	for (size_t i = 0; i < c->size; ++i) {
-		printf("%d\n", container_geti(c, i));
+	for (size_t i = 0; i < container->size; ++i) {
+		printf("%f\n", container_getd(container, i));
 	}
-	printf("Size: %lu\n\n", c->size);
+	printf("Size: %lu\n\n", container->size);
 
-	container_delete(&c);
+	container_assign(&container, 7.123, 0);
+
+	for (size_t i = 0; i < container->size; ++i) {
+		printf("%f\n", container_getd(container, i));
+	}
+	printf("Size: %lu\n\n", container->size);
+
+	container_delete(&container);
 	return 0;
 
 	/*
